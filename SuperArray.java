53e358807a540fc8e2fcbe8354f0afebcad982b6
Reshmi-Anwar <ranwar10@stuy.edu>
2020-11-03 15:09:03 -0500

    methods including resize

--- SuperArray.java
+++ SuperArray.java
@@ -2,7 +2,33 @@ class SuperArray{
   private String [] data;
   private int size;
   public SuperArray(){
-    data = newString[10];
+    data = new String[10];
     size = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(String element){
+    if(data.length == size){
+      resize();
+    }
+    data[size] = element;
+    size += 1;
+    return true;
+  }
+  public String get(int index){
+    return data[index];
+  }
+  public String set(int index, String element){
+    String x = data[index];
+    data[index] = element;
+    return x;
+  }
+  private void resize(){
+    String[] y = new String[data.length + 1];
+    for(int i = 0; i < data.length; i++){
+      y[i] = data[i];
+      data = y;
+    }
   }
 }
