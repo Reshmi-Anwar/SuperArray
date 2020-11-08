b3de8315809e64c565feff8cb74be7fc00becda1
Reshmi-Anwar <ranwar10@stuy.edu>
2020-11-08 15:13:44 -0500

    last index of

--- Demo.java
+++ Demo.java
@@ -1,4 +1,6 @@
 public class Demo{
+  private String [] data;
+  private int size;
   public static void removeDuplicates(SuperArray s){
     for(int i = 0; i < s.size(); i++){
       if(i != s.indexOf(s.get(i))){
@@ -6,6 +8,17 @@ public class Demo{
         i--;
       }
     }
+  }
+  //public static SuperArray findOverlap(SuperArray a, SuperArray b){
+
+  //}
+  public int lastIndexOf(String s){
+    for(int i = size - 1; i >= 0; i++){
+      if (data[i].equals(s)){
+        return i;
+      }
+    }
+    return -1;
   }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
